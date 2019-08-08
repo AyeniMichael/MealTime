3fbf6987f7c16a1e012b90ba4335adff2f1b0f29
Ayeni Michael <ayenimichael1998@gmail.com>
2019-08-08 16:19:49 +0100

    add the boilerplate file and a C# MealTime program file

---
+++ Meal.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Mealtime
+{
+    static void Main()
+    {
+        public WhatAreYouEating(string meal)
+        {
+            Console.WriteLine("What are you eating for "+meal+".");
+            string yourMeal = Console.ReadLine();
+            Console.WriteLine("You had "+yourMeal+"for "+meal+".");
+        }
+    }
+}
